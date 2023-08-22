8d416b52a14ee04eaa579e3757e20b61e5c1b105
Denis Masalskii <masdv973@gmail.com>
2023-08-22 11:20:17 +0300

    Добавить WebConfig

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -16,7 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(HelloController.class)
+@WebMvcTest()
 public class HelloControllerTest {
     @Autowired
 	private MockMvc mockMvc;
