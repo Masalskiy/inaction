1043dfc2607b6f24fe9932c6322273c043924f38
Denis Masalskii <masdv973@gmail.com>
2023-08-22 10:52:57 +0300

    Добавить валидацию форм дла заказа и для тако

--- src/main/java/com/example/springboot/OrderController.java
+++ src/main/java/com/example/springboot/OrderController.java
@@ -1,14 +1,16 @@
 package com.example.springboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Slf4j
@@ -20,8 +22,12 @@ public class OrderController {
     public String orderForm() {
         return "orderForm";
     }
+
     @PostMapping
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus ){
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus ){
+        if(errors.hasErrors()){
+            return "orderForm";
+        }
         log.info("Order submitted: {}", order);
         return "redirect:/";
     }
